1152773ebca0941f0c7eae38d7136cff618ad73c
kholishafid <hafidnkholis@gmail.com>
2025-06-09 10:07:24 +0700

    ⬆️upgrade tailwindcss to v4 & change path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from './routes/root'
-import Error from './routes/error'
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+// import Root from './routes/root'
+// import Error from './routes/error'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <Error />
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//     errorElement: <Error />
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Root />
   </React.StrictMode>,
 )
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,4 @@
-import ReactLogo from '~/assets/react.svg'
+import ReactLogo from '@/assets/react.svg'
 
 const Root = () => {
 	return (
